efd5da4569b12b2ae069a1d8f55ced543702bd20
ashish singh Chauhan <ashish@interactionone.com>
2018-09-11 23:12:04 +0530

    Added cases.

--- app/src/test/java/com/example/ashish/task/PresenterTests.kt
+++ app/src/test/java/com/example/ashish/task/PresenterTests.kt
@@ -7,19 +7,17 @@ import com.example.ashish.task.adapter.ItemsAdapter
 import com.example.ashish.task.model.RowData
 import com.example.ashish.task.presenter.GetDataInterface
 import com.example.ashish.task.presenter.PresenterLogic
+import com.nhaarman.mockito_kotlin.spy
 import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mock
-import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 
 
 class PresenterTests {
 
 
-
-    @Mock
+    //@Mock
     private var view: GetDataInterface.View? = null
     private var presenter: PresenterLogic? = null
     private var mockmainActivity: MainActivity? = null
@@ -29,38 +27,36 @@ class PresenterTests {
     private var recyclerView: RecyclerView? = null
     private var mockViewHolder: ItemsAdapter.ViewHolder? = null
 
-
-    @Test
-    fun testSmallTest() {
-        val activity = MainActivity()
-        assertNotNull("MainActivity is not available", activity)
-    }
-
     @Before
     @Throws(Exception::class)
     fun setUp() {
         MockitoAnnotations.initMocks(this)
-        context = mockmainActivity
-        // Make presenter a mock while using mock repository and viewContract created above
-        presenter = Mockito.spy<PresenterLogic>(PresenterLogic(this.view!!))
         mockmainActivity = MainActivity()
-        mockAdapter = ItemsAdapter(context!!, list)
-    }
-
-    @Test
-    fun testForAdapterItems() {
-        val rowData = ArrayList<RowData>()
-        Mockito.`when`(mockAdapter!!.itemCount).thenReturn(rowData.size)
-
+        context = mockmainActivity
+        list = spy()
     }
 
 
+    @Test
+    fun testSmallTest() {
+        assertNotNull("MainActivity is not available", mockmainActivity)
+    }
 
     @Test
-    fun defaultDisplay() {
-        val recyclerView = mockmainActivity!!.findViewById(R.id.rv) as RecyclerView
-        val layoutManager = recyclerView.layoutManager
-        Mockito.verify(layoutManager)
+    fun testForAdapter() {
+        val adapter = ItemsAdapter(context!!, list)
+        assertNotNull("", adapter)
+    }
+
+    @Test
+    fun checkListInAdapter() {
+        val adapter = ItemsAdapter(context!!, list)
+        assertNotNull("", adapter.list)
+    }
+
+    @Test
+    fun checkTitle() {
+        assertNotNull("", mockmainActivity!!.title)
     }
 
 
