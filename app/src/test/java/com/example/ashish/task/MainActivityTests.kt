6d914033754e59a1cb60634c6c2d7b91336480b2
ashish singh Chauhan <ashish@interactionone.com>
2018-09-12 15:44:33 +0530

    Added unit test cases.

---
+++ app/src/test/java/com/example/ashish/task/MainActivityTests.kt
@@ -0,0 +1,77 @@
+package com.example.ashish.task
+
+import android.support.v7.widget.LinearLayoutManager
+import com.example.ashish.task.adapter.ItemsAdapter
+import com.example.ashish.task.model.RowData
+import com.example.ashish.task.presenter.PresenterLogic
+import com.example.ashish.task.receiver.ConnectionReceiver
+import com.example.ashish.task.receiver.MainApplication
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+
+class MainActivityTests {
+
+    @Mock
+    private var mockmainActivity: MainActivity? = null
+    private var isConnected: Boolean = true
+    private var presenter: PresenterLogic? = null
+    private var layoutManager: LinearLayoutManager? = null
+    private var adapter: ItemsAdapter? = null
+    private var list: ArrayList<RowData> = ArrayList()
+    private var mockMainApplication: MainApplication? = null
+    private var connectionReceiver: ConnectionReceiver? = null
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        //MockitoAnnotations.initMocks(this)
+        mockmainActivity = MainActivity()
+        layoutManager = mockmainActivity!!.layoutManager
+        adapter = ItemsAdapter(mockmainActivity!!, list)
+        mockMainApplication = MainApplication()
+        connectionReceiver = ConnectionReceiver()
+    }
+
+    @Test
+    fun testForMainActivity() {
+        Assert.assertNotNull("MainActivity is not available", mockmainActivity)
+    }
+
+
+    @Test
+    fun testForNetwork() {
+        mockmainActivity!!.onNetworkConnectionChanged(isConnected)
+        Assert.assertTrue("Don't show snackBar", isConnected)
+    }
+
+    @Test
+    fun testForViewMethods() {
+        Assert.assertTrue("onGetDataSuccess() called", (presenter != null))
+    }
+
+    @Test
+    fun testForLayoutManager() {
+        Assert.assertNull("LayoutManager not null", layoutManager)
+    }
+
+    @Test
+    fun testLoadImageGlide() {
+        Assert.assertFalse("Don't call load image", mockmainActivity == null)
+    }
+
+    @Test
+    fun testInstanceOfMainApplication() {
+        Assert.assertTrue("Don't call connection listener", (MainApplication.getInstance() == null))
+    }
+
+    @Test
+    fun testCallInstance() {
+        Assert.assertTrue("Don't call get instance", (MainApplication.getInstance() != null))
+    }
+
+
+
+
+}
--- app/src/test/java/com/example/ashish/task/PresenterTests.kt
+++ app/src/test/java/com/example/ashish/task/PresenterTests.kt
@@ -4,20 +4,38 @@ package com.example.ashish.task
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import com.example.ashish.task.adapter.ItemsAdapter
+import com.example.ashish.task.model.ResponseData
 import com.example.ashish.task.model.RowData
+import com.example.ashish.task.network.ApiContract
+import com.example.ashish.task.network.RestClient
 import com.example.ashish.task.presenter.GetDataInterface
 import com.example.ashish.task.presenter.PresenterLogic
 import com.nhaarman.mockito_kotlin.spy
 import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
+import org.mockito.Mock
 import org.mockito.MockitoAnnotations
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mockito.`when`
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import okhttp3.OkHttpClient
+
+
+
+
+
+
 
 
 class PresenterTests {
 
 
-    //@Mock
+    @Mock
+    private var mockGetDataView: GetDataInterface.View ?=null
     private var view: GetDataInterface.View? = null
     private var presenter: PresenterLogic? = null
     private var mockmainActivity: MainActivity? = null
@@ -26,6 +44,7 @@ class PresenterTests {
     private var context: Context? = null
     private var recyclerView: RecyclerView? = null
     private var mockViewHolder: ItemsAdapter.ViewHolder? = null
+    private var mockApiContract : ApiContract ?= null
 
     @Before
     @Throws(Exception::class)
@@ -33,7 +52,15 @@ class PresenterTests {
         MockitoAnnotations.initMocks(this)
         mockmainActivity = MainActivity()
         context = mockmainActivity
+        mockGetDataView = spy()
+        presenter = PresenterLogic(mockGetDataView!!)
+
         list = spy()
+    }
+
+    @Test
+    fun testGetView(){
+        assertNotNull("GetDataView not null in presenter",mockGetDataView)
     }
 
 
@@ -58,6 +85,8 @@ class PresenterTests {
     fun checkTitle() {
         assertNotNull("", mockmainActivity!!.title)
     }
+
+
 
 
 }
