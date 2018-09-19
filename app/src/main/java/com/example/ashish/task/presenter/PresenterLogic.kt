3c24ea2f4d66a3decb84a6fe7863abe268f23ecd
ashish singh Chauhan <ashish@interactionone.com>
2018-09-19 17:04:08 +0530

    Fixed image loading issue.

--- app/src/main/java/com/example/ashish/task/presenter/PresenterLogic.kt
+++ app/src/main/java/com/example/ashish/task/presenter/PresenterLogic.kt
@@ -1,39 +1,26 @@
 package com.example.ashish.task.presenter
 
-import android.content.Context
-import android.widget.Toast
-import com.example.ashish.task.R
-import com.example.ashish.task.model.ResponseData
-import com.example.ashish.task.network.RestClient
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.example.ashish.task.model.GetData
+import com.example.ashish.task.model.RowData
+import java.util.*
 
 class PresenterLogic(var mGetDataView: GetDataInterface.View) :
-        GetDataInterface.Presenter {
+        GetDataInterface.Presenter, GetDataInterface.onGetDataListener {
 
 
-    override fun getDataFromURL(context: Context) {
-        RestClient.getClient().getData().enqueue(object : Callback<ResponseData> {
-            override fun onResponse(p0: Call<ResponseData>?, response: Response<ResponseData>) {
-                if (response.isSuccessful) {
-                    val responseBody = response.body()
-                    if (responseBody != null) {
-                        mGetDataView.onGetDataSuccess("", responseBody.rows, responseBody.title)
-                    } else {
-                        Toast.makeText(context, context?.getString(R.string.no_data), Toast.LENGTH_LONG).show()
+    var getData: GetData = GetData(this)
 
-                    }
 
-                }
-            }
+    override fun getDataFromURL() {
+        getData.initRetrofitCall()
+    }
 
-            override fun onFailure(p0: Call<ResponseData>?, p1: Throwable?) {
-                mGetDataView.onGetDataFailure(p1?.message!!)
+    override fun onSuccess(message: String, list: ArrayList<RowData>, title: String) {
+        mGetDataView.onGetDataSuccess(message, list, title)
+    }
 
-            }
-        })
-
+    override fun onFailure(message: String) {
+        mGetDataView.onGetDataFailure(message)
     }
 
 }
