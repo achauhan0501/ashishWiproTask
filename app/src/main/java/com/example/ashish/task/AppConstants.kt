a6e65439fd4a1e064337005cb9af742403aaac3f
ashish singh Chauhan <ashish@interactionone.com>
2018-09-23 21:56:39 +0530

    Required Changes.

--- app/src/main/java/com/example/ashish/task/AppConstants.kt
+++ app/src/main/java/com/example/ashish/task/AppConstants.kt
@@ -1,11 +1,14 @@
 package com.example.ashish.task
 
+import android.os.Bundle
+
 class AppConstants {
 
     companion object {
         val BASE_URL: String = "https://dl.dropboxusercontent.com/"
         val SAVED_RECYCLER_VIEW_DATASET: String = "list"
         val SAVED_RECYCLER_VIEW_HEADING: String = "title"
+
 
     }
 }
